2237548eabd4d96aa215bebd4fcfcb6f95634cca
Connor Cowan <ccowan003@comcast.net>
2019-06-10 21:15:18 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,7 +3,7 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-    return collection[i] if yield(collection[i])
+    return collection[i] if yield(collection[i])      #if block returns true, return element we passed into yeild
     i += 1
   end
 end
